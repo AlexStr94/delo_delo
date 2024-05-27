62124e1624a463c09282214cb0583c697a38ef35
AlexStr94 <alex.strunskiy@ya.ru>
2024-05-27 10:47:17 +0300

    delete shaket fab

--- components/AddTaskFAB.js
+++ components/AddTaskFAB.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { FAB, Portal} from 'react-native-paper';
+import { FAB, Portal } from 'react-native-paper';
 
-import {AddGoalScreenName, AddCurrentTaskScreenName, AddPeriodicalTaskScreenName } from '../constants'
+import { AddGoalScreenName, AddCurrentTaskScreenName, AddPeriodicalTaskScreenName } from '../constants'
 
-const AddTaskFAB = () => {
+export default AddTaskFAB = () => {
   const [state, setState] = React.useState({ open: false });
 
   const onStateChange = ({ open }) => setState({ open });
@@ -14,35 +14,31 @@ const AddTaskFAB = () => {
   const navigation = useNavigation();
 
   return (
-		<Portal>
-			<FAB.Group
-				open={open}
-				visible
-				icon='plus'
-				style={{bottom: 80}}
-        		backdropColor='transparent'
-				theme={{ colors: { backdrop: 'transparent' } }}
-				actions={[
-					{
-						icon: 'bullseye-arrow',
-						label: 'Цели',
-						onPress: () => navigation.navigate(AddGoalScreenName),
-					},
-					{
-						icon: 'bee',
-						label: 'Текущие дела',
-						onPress: () => navigation.navigate(AddCurrentTaskScreenName),
-					},
-					{
-						icon: 'calendar-sync',
-						label: 'Периодические дела',
-						onPress: () => navigation.navigate(AddPeriodicalTaskScreenName),
-					},
-				]}
-				onStateChange={onStateChange}
-			/>
-		</Portal>
+    <Portal>
+      <FAB.Group
+        open={open}
+        visible
+        icon='plus'
+        actions={[
+          {
+            icon: 'bullseye-arrow',
+            label: 'Цели',
+            onPress: () => navigation.navigate(AddGoalScreenName),
+          },
+          {
+            icon: 'bee',
+            label: 'Текущие дела',
+            onPress: () => navigation.navigate(AddCurrentTaskScreenName),
+          },
+          {
+            icon: 'calendar-sync',
+            label: 'Периодические дела',
+            onPress: () => navigation.navigate(AddPeriodicalTaskScreenName),
+          },
+        ]}
+        onStateChange={onStateChange}
+      />
+    </Portal>
   );
 };
 
-export default AddTaskFAB;
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -2,7 +2,6 @@ import { ScrollView } from 'react-native'
 import { List, Portal } from 'react-native-paper';
 
 import AddTaskFAB from '../components/AddTaskFAB'
-import GetTaskFAB from '../components/GetTaskFAB'
 import ListWithGoals from '../components/ListWithGoals'
 import ListWithCurrentTasks from '../components/ListWithCurrentTasks'
 import ListWithPeriodicalTasks from '../components/ListWithPeriodicalTasks'
@@ -13,29 +12,28 @@ export default MainScreen = () => {
     <Portal.Host>
       <ScrollView>
         <List.AccordionGroup>
-          <List.Accordion 
+          <List.Accordion
             left={props => <List.Icon {...props} icon="bullseye-arrow" />}
-            title="Цели" 
+            title="Цели"
             id="1"
           >
             <ListWithGoals />
           </List.Accordion>
-          <List.Accordion 
+          <List.Accordion
             left={props => <List.Icon {...props} icon="bee" />}
-            title="Текущие дела" 
+            title="Текущие дела"
             id="2"
           >
-            <ListWithCurrentTasks/>
+            <ListWithCurrentTasks />
           </List.Accordion>
-          <List.Accordion 
+          <List.Accordion
             left={props => <List.Icon {...props} icon="calendar-sync" />}
-            title="Периодические дела" 
+            title="Периодические дела"
             id="3"
           >
             <ListWithPeriodicalTasks />
           </List.Accordion>
         </List.AccordionGroup>
-        <GetTaskFAB></GetTaskFAB>
         <AddTaskFAB />
       </ScrollView>
     </Portal.Host>
